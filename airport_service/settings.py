a14328316ff3ba2f5c8b714c01cff01fe4a98041
tiway <stilon670@gmail.com>
2024-06-23 18:08:02 +0300

    Create app User and add to settings.py

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "flights",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
