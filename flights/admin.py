156461a3112505a43949f4fe1778e7e628adde6f
tiway <stilon670@gmail.com>
2024-06-24 11:14:24 +0300

    added fields to the admin panel in flight > admin.py

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from flights.models import Ticket, Flight, Order, Crew, AirplaneType, Airport, Airplane
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
+admin.site.register(Flight)
+admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(AirplaneType)
+admin.site.register(Crew)
+admin.site.register(Ticket)
