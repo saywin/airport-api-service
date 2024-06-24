e75c5b1d2bfccb1fa8f54045cb2da5d642cc521b
tiway <stilon670@gmail.com>
2024-06-24 10:49:59 +0300

    Create simple ViewsSet for all models to fights > views.py

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from flights.models import Airport, Flight, AirplaneType, Route, Crew, Airplane, Order
+from flights.serializers import (
+    AirportSerializer,
+    FlightSerializer,
+    AirplaneTypeSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    AirplaneSerializer, OrderSerializer
+)
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
