8ffc8cde4eab2c8a1ba241e8c65b0d11016c7616
tiway <stilon670@gmail.com>
2024-06-27 13:41:13 +0300

    Add ManageUserView to user > views

--- user/views.py
+++ user/views.py
@@ -1,7 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
