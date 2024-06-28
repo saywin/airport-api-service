40731b78092f11dade1df0d87cf0924f2f726be0
tiway <stilon670@gmail.com>
2024-06-28 11:50:13 +0300

    add path schema, swagger, redoc to urls.py

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -18,11 +18,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/airport/", include("flights.urls", namespace="airport")),
     path("api/v1/user/", include("user.urls", namespace="user")),
+
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/v1/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
