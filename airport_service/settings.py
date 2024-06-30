622fe3da738121cf067aa0080fad4089ccf19257
tiway <stilon670@gmail.com>
2024-06-30 15:42:19 +0300

    add .env.sample with SECRET_KEY

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!=s)!8q0t#-g9z1f-sx-+nke#$=rubgb@3%-u99=(%u%bo^spb"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
